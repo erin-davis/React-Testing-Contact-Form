55caca57d7880ed9d968907a991b1702c482c786
Erin Davis <davisaerin@gmail.com>
2020-05-28 19:27:55 -0500

    did not super understand the objective here but I did some debugging until jest stopped working.

--- src/App.test.js
+++ src/App.test.js
@@ -3,5 +3,21 @@ import { render } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
-  render(<App />);
+  return render(<App />);
 });
+
+test("First Name field renders", () =>{
+  const container = render(<App />);
+  container.getByText(/first name/i);
+});
+
+test('All Placeholders Show Up', ()=>{
+  const {getByPlaceholderText} = render(<App />);
+  getByPlaceholderText("Burke");
+  getByPlaceholderText("Edd");
+  getByPlaceholderText(/bluebill1049@hotmail.com/i);
+})
+
+test("Make sure there's no length restrictions", ()=>{
+  expect(maxLength).toBeGreaterThan(0);
+})
